f06bcb5b993bc96b83b5e5bc054d86630a72e477
Prabhu <vprabhu1771@gmail.com>
2025-02-12 19:48:05 +0530

    Constants api routes added

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -4,15 +4,29 @@ class Constants {
 
   static const String BASE_URL = SERVER_DOMAIN + "/api";
 
-  static const String CATEGORY_ROUTE = '/categories';
+  static const String LOGIN_ROUTE = "/login";
 
-  static const String LOGIN_ROUTE = "/sanctum/token";
+  static const String LOGOUT_ROUTE = "/logout";
 
-  static const String LOGOUT_ROUTE = "/user/revoke";
+  static const String USER_ROUTE = "/user";
 
   static const String USER_REGISTER_ROUTE = "/register";
 
-  static const String USER_INFO_ROUTE = "/user";
+  static const String USER_PROFILE_PIC_UPLOAD_ROUTE = "/upload profile pic";
 
-  static const String USER_PROFILE_PIC_UPLOAD_ROUTE = "/upload-profile-pic";
+  static const String IMAGE_ERROR_ROUTE = SERVER_DOMAIN + "/storage";
+
+  static const String NOTIFICATION_ROUTE = '/notifications';
+
+  static const String CATEGORY_ROUTE = '/categories';
+
+  static const String SERVICES_ROUTE = '/services?category_id=';
+
+  static const String SETTINGS_ROUTE = '/settings';
+
+  static const String FILTER_BY_SERVICE = '/services/filter?service_id=';
+
+  static const String SUBMIT_BOOKING_ROUTE = '/submit-appointment';
+
+  static const String SUBMIT_FEEDBACK_ROUTE = "/submit-feedback";
 }
